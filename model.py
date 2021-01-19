794248bc9ddbb11dd51b33ed563810a20ea13a29
Rafael Castro <rcgonzsv@aol.com>
2021-01-19 00:40:16 -0600

    v0.1.0

--- model.py
+++ model.py
@@ -114,7 +114,7 @@ if __name__ == '__main__':
 	                                    samples_per_epoch = len(train_samples),
 	                                    validation_data = validation_generator,
 	                                    nb_val_samples = len(validation_samples),
-	                                    nb_epoch = 3,
+	                                    nb_epoch = 7,
 	                                    verbose = 1)
 	print('Training Completed...')
 
